2fa09612a273561e14b9aa024b723799fb3a17f8
lvsongmei <lvsongmei@yy.com>
2018-11-06 16:36:58 +0800

    remove process name import

--- api_test/main.py
+++ api_test/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import setproctitle
+#import setproctitle
 import logging
 import time
 import os
@@ -14,14 +14,14 @@ import handlers
 
 define("port", 35000, int, "Listen port")
 define("address", "0.0.0.0", str, "Bind address")
-define("psname", "mail_local_srv_d", str, "Back run process name")
+#define("psname", "mail_local_srv_d", str, "Back run process name")
 define("localversion", 0, int, "local version")
 define("autoreload", 0, int, "Enable autoreload, 0 is disable")
 
 # Parse commandline
 tornado.options.parse_command_line()
 # Set process title
-setproctitle.setproctitle(options.psname)
+#setproctitle.setproctitle(options.psname)
 
 webapp = Application(
     [
